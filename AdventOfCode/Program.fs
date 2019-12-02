10a9a122e229575262f7ecf71224587dee232d79
Erik Schierboom <erik_schierboom@hotmail.com>
2019-12-02 21:28:39 +0100

    Add day 1

---
+++ AdventOfCode/Program.fs
@@ -0,0 +1,24 @@
+﻿module AdventOfCode.Program
+
+open AdventOfCode.Data
+
+let fuelRequired mass =
+    if mass <= 0 then 0
+    else mass / 3 - 2
+
+let fuelRequiredRecursively mass =
+    let rec helper total remainder =
+        let fuel = fuelRequired remainder
+        if fuel <= 0 then total
+        else helper (total + fuel) fuel
+
+    helper 0 mass
+
+let solveOne = List.sumBy fuelRequired masses
+let solveTwo = List.sumBy fuelRequiredRecursively masses
+
+[<EntryPoint>]
+let main _ =
+    printfn "One: %A" solveOne
+    printfn "Two: %A" solveTwo
+    0
